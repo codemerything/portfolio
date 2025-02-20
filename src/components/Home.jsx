30e82e539009423a318cb76aa0936475caaf09f4
Emery <mremmanueledem@gmail.com>
2025-02-20 21:16:47 +0100

    misc: add template files

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <nav>
+      <ul className="flex ">
+        <li>Testing</li>
+        <li>Testing</li>
+        <li>Testing</li>
+        <li>Testing</li>
+      </ul>
+    </nav>
+  );
+}
