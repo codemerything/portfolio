a9bcc1c5d037d39e69cc27043391802cb59e620e
Emery <mremmanueledem@gmail.com>
2025-03-31 04:01:53 +0100

    feat: add overall style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Tester from "../components/Tester"
 
 export default function Home() {
   return (
-    <main className="text-lightGray mx-14 my-5">
+    <main className="text-lightGray mx-auto my-5 w-full max-w-screen-sm px-8 md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-2xl">
       <Navbar />
       <Tester />
     </main>
